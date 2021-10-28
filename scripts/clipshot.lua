0b2e2cc43d82f4eb79fd5f93d7705aa173ff7008
zk <liuzq7@gmail.com>
2021-10-29 00:44:08 +0800

    feat: 增加 mpv 截屏

---
+++ scripts/clipshot.lua
@@ -0,0 +1,43 @@
+NAME = 'mpv-screenshot.jpeg'
+
+if package.config:sub(1, 1) == '\\' then -- Windows
+    SHOT = os.getenv('TEMP')..'\\'..NAME
+    CMD = {
+        'powershell', '-NoProfile', '-Command', ([[& {
+            Add-Type -Assembly System.Windows.Forms;
+            Add-Type -Assembly System.Drawing;
+            $shot = [Drawing.Image]::FromFile(%q);
+            [Windows.Forms.Clipboard]::SetImage($shot);
+        }]]):format(SHOT)
+    }
+else -- Unix
+    SHOT = '/tmp/'..NAME
+    -- os.getenv('OSTYPE') doesn't work
+    local ostype = io.popen('printf "$OSTYPE"', 'r'):read()
+    if ostype:sub(1, 6) == 'darwin' then -- MacOS
+        CMD = {
+            'osascript', '-e', ([[¬
+                set the clipboard to ( ¬
+                    read (POSIX file %q) as JPEG picture ¬
+                ) ¬
+            ]]):format(SHOT)
+        }
+    else -- Linux/BSD
+        if os.getenv('XDG_SESSION_TYPE') == 'wayland' then -- Wayland
+            CMD = {'sh', '-c', ('wl-copy < %q'):format(SHOT)}
+        else -- Xorg
+            CMD = {'xclip', '-sel', 'c', '-t', 'image/jpeg', '-i', SHOT}
+        end
+    end
+end
+
+function clipshot(arg)
+    return function()
+        mp.commandv('screenshot-to-file', SHOT, arg)
+        mp.command_native({'run', unpack(CMD)})
+    end
+end
+
+mp.add_key_binding('C',     'clipshot-subs',   clipshot('subtitles'))
+mp.add_key_binding('c',     'clipshot-video',  clipshot('video'))
+-- mp.add_key_binding('Alt+c', 'clipshot-window', clipshot('window'))
